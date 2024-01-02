3d09af816872ed4728ba48a710b0cc91e95c8731
snowypark <partsnowy@gmail.com>
2024-01-02 17:10:15 +0900

    ch7 : git 실습

--- src/main/java/com/study/ch04/Operation07.java
+++ src/main/java/com/study/ch04/Operation07.java
@@ -3,7 +3,7 @@ package com.study.ch04;
 public class Operation07 {
     public static void main(String[] args) {
 
-        System.out.println("깃, 깃허브 수업 실습");
+        System.out.println("깃, 깃허브 수업 실습"); //기존 pc
 
     }
 }
