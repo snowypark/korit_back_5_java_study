22399204c42da67c63178a00e3ce8e1f87283aa9
snowypark <partsnowy@gmail.com>
2024-01-03 09:01:43 +0900

    test

--- src/main/java/com/study/ch04/Operation07.java
+++ src/main/java/com/study/ch04/Operation07.java
@@ -4,6 +4,7 @@ public class Operation07 {
     public static void main(String[] args) {
 
         System.out.println("깃, 깃허브 수업 실습"); //기존 pc
+        System.out.println("깃, 깃허브 수업 실습"); //기존 pc
 
     }
 }
