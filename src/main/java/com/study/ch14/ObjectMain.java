fce96a04e2a9f3307e0cc58ea3f01497725d6bf1
snowypark <partsnowy@gmail.com>
2024-01-10 10:14:40 +0900

    obejct

--- src/main/java/com/study/ch14/ObjectMain.java
+++ src/main/java/com/study/ch14/ObjectMain.java
@@ -3,11 +3,24 @@ package com.study.ch14;
 public class ObjectMain {
     public static void main(String[] args) {
 
-        ObjectTest objectTest = new ObjectTest();
+        ObjectTest objectTest = new ObjectTest("김준일", "동래구");
         System.out.println(objectTest.toString());
         System.out.println(objectTest); //toString 자동 호출
 
+        ObjectTest objectTest1 = new ObjectTest("김준일", "동래구");
+        System.out.println(objectTest.equals(objectTest1)); // 주소 다름. 다른 객체.
+        System.out.println(objectTest == objectTest1);      // equals 와 같은 기능.
+        // equals 로 확인한 값 일치 true / ==로 확인한 주소 비교 false
 
+        System.out.println("----------");
+
+        System.out.println(objectTest.getClass());
+        System.out.println(objectTest1.getClass());  //
+        System.out.println(ObjectTest.class);        // static 접근
+
+        System.out.println(objectTest.hashCode());  // hashCode = 실제 메로리 주소
+        System.out.println(objectTest1.hashCode()); // Integer.toHexString(hashCode() = 정수->십육진수->십진수
+                                                    // 주소값 비교 = 오버라이드 후 일치(값 일치)
 
     }
 }
--- src/main/java/com/study/ch14/ObjectTest.java
+++ src/main/java/com/study/ch14/ObjectTest.java
@@ -4,13 +4,43 @@ package com.study.ch14;
 
  */
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class ObjectTest {
+    private String name;
+    private String address;
+
+    public ObjectTest(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
 
     @Override
     public String toString() {
         return "toString 메소드 테스트";
+    }
+
+//    @Override
+//    public boolean equals(Object obj) { // 주소가 일치하는지 확인.
+//        return super.equals(obj);
+//    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true; //주소 같으면 true
+        if (o == null || getClass() != o.getClass()) return false;  // 자기자신확인. this 생략. object.getClass(서로 다른 객체)
+                                                                    // 주소가 다른데 한쪽이 null
+        ObjectTest that = (ObjectTest) o;                           // 모든객체는 object로 업캐스팅 가능 -> 다운캐스팅 대입
+        return Objects.equals(name, that.name) && Objects.equals(address, that.address);
+        // Object+s -> s가 붙으면 도구로 쓰이는 클래스. static method. 생성 없이 사용 가능
+        // 2개의 문자열 비교 -> return true & false
+        // 주소가 달라도 값이 같으면 같다.
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, address);     // 문자열(유니코드)->정수형 해쉬값
     }
 
 }
